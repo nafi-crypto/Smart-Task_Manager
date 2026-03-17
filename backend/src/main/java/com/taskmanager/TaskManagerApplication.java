dc4523b78e56b153b934fcc74ddd2318fc80ff04
shaik_Nafi <shaiknafi04@gmail.com>
2026-03-17 13:11:49 +0530

    updated taskmanager

--- backend/src/main/java/com/taskmanager/TaskManagerApplication.java
+++ backend/src/main/java/com/taskmanager/TaskManagerApplication.java
@@ -2,10 +2,19 @@ package com.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TaskManagerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running successfully 🚀";
     }
 }
